ba2274d72c5e522cbb015754e8ab10341923ff39
sumibaktha <sumibaktha@gmail.com>
2023-12-03 06:58:27 -0500

    Portfolio with case study Initial

---
+++ js/index.js
@@ -0,0 +1,48 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+});
+
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=wtqbOFYivxw");
+});
+
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", () => {
+        window.open("https://www.youtube.com/watch?v=H5tuwAO-_Kg");
+});
+
+document.getElementsByClassName("article")[0].addEventListener("click", () => {
+        window.open("https://iamcodefoxx.github.io/Weather-App/");
+});
+
+document.getElementsByClassName("article")[1].addEventListener("click", () => {
+        window.open("https://iamcodefoxx.github.io/Quiz-App/");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", () => {
+        window.open("https://iamcodefoxx.github.io/Marvel-API/");
+});
+
+$(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+      
+          // Make sure this.hash has a value before overriding default behavior
+          if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+      
+            // Store hash
+            var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 800, function(){
+         
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+      });
